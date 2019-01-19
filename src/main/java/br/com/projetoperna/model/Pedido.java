66cafe65b581e96b8368c735a3c89043d726de30
legionario07 <paulinho.legionario@hotmail.com>
2019-01-19 19:11:31 -0200

    Criado desconto em classe pedido

--- src/main/java/br/com/projetoperna/model/Pedido.java
+++ src/main/java/br/com/projetoperna/model/Pedido.java
@@ -2,14 +2,9 @@ package br.com.projetoperna.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,9 +29,18 @@ public class Pedido implements Serializable {
 	@OneToOne
 	private Produto produto;
 	private Integer total;
+	private BigDecimal desconto;
 
 	
 	
+	public BigDecimal getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(BigDecimal desconto) {
+		this.desconto = desconto;
+	}
+
 	public Pedido(BigDecimal valorTotal, Produto produto, Integer total) {
 		super();
 		this.valorTotal = valorTotal;
