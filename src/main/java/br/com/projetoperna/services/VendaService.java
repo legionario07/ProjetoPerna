495e58e17240881cbbce738c53ec1f35f2e399c0
legionario07 <paulinho.legionario@hotmail.com>
2019-01-25 12:46:29 -0200

    Venda ira salvar todos os pedidos antes de Persistir

--- src/main/java/br/com/projetoperna/services/VendaService.java
+++ src/main/java/br/com/projetoperna/services/VendaService.java
@@ -3,16 +3,14 @@ package br.com.projetoperna.services;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.projetoperna.model.Produto;
+import br.com.projetoperna.model.Pedido;
 import br.com.projetoperna.model.Venda;
+import br.com.projetoperna.repository.PedidoRepository;
 import br.com.projetoperna.repository.ProdutoRepository;
 import br.com.projetoperna.repository.VendaRepository;
 
@@ -25,6 +23,9 @@ public class VendaService {
 	
 	@Autowired
 	private ProdutoRepository produtoRepo;
+	
+	@Autowired
+	private PedidoRepository pedidoRepo;
 
 	public Venda find(Long id) {
 		return vendaRepo.findById(id).get();
@@ -49,41 +50,13 @@ public class VendaService {
 
 	public Venda save(Venda venda) {
 		
-		/**
-		 * Existem produtos que podem ser divididos em X unidades, 
-		 * se essas X unidades acabarem deverá ser decrementado uma unidade do PRODUTO
-		 * Deverá ser verificado se é ultima unidade de X para decrementar
-		 */
-		
-//		Map<Produto, Integer> mapProdutos = new HashMap<Produto, Integer>();
-//		
-//		//É um subProduto, se for adiciona no map com sua frequencia
-//		for(Produto p : venda.getProdutos()) {
-//			if(p.isSubProduto()) {
-//				if(mapProdutos.containsKey(p)) {
-//					Integer valor = mapProdutos.get(p);
-//					mapProdutos.put(p, valor++);
-//				}else {
-//					mapProdutos.put(p, 1);
-//				}
-//			}
-//		}
-//		
-//		Set<Produto> keys = mapProdutos.keySet();
-//		
-//		for(Produto produtoKey : keys) {
-//			Integer total = mapProdutos.get(produtoKey);
-//			if(total>=produtoKey.getQteDivisao()) {
-//				Integer valorADecrementar = total/produtoKey.getQteDivisao();
-//				produtoKey.decrementarProduto(valorADecrementar);
-//				
-//				produtoRepo.save(produtoKey);
-//				
-//			}
-//			
-//		}
 		
 		try {
+			
+			for(Pedido pedido : venda.getPedidos()) {
+				pedidoRepo.save(pedido);
+			}
+			
 		 venda = vendaRepo.save(venda);
 		}catch(Exception e) {
 			return null;
